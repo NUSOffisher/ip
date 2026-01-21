559ce5657fdbee2b72c990897008993e413e2d0d
Chan Jan Hoe <e1398771@u.nus.edu>
2026-01-22 00:08:35 +0800

    Added level 1

--- src/main/java/Tigger.java
+++ src/main/java/Tigger.java
@@ -1,10 +1,24 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Tigger {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________\n");
         System.out.println("Hello! I'm Tigger\n");
         System.out.println("What can I do for you?\n");
         System.out.println("____________________________________________________________\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+
+        while (!Objects.equals(command, "bye")) {
+            System.out.println("    ____________________________________________________________\n");
+            System.out.println("    " + command);
+            System.out.println("    ____________________________________________________________\n");
+            command = scanner.nextLine();
+        }
+        System.out.println("    ____________________________________________________________\n");
+        System.out.println("    Bye. Hope to see you again soon!\n");
+        System.out.println("    ____________________________________________________________");
     }
 }
