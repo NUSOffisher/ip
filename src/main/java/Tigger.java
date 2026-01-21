8cd42592156f0168f3ed6c4d875b676d570f06f5
Chan Jan Hoe <e1398771@u.nus.edu>
2026-01-22 00:59:29 +0800

    Added level 3

--- src/main/java/Tigger.java
+++ src/main/java/Tigger.java
@@ -9,7 +9,7 @@ public class Tigger {
         System.out.println("What can I do for you?\n");
         System.out.println("____________________________________________________________\n");
 
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
@@ -17,12 +17,30 @@ public class Tigger {
         while (!Objects.equals(command, "bye")) {
             if (command.equals("list")){
                 System.out.println("    ____________________________________________________________\n");
+                System.out.println("    Here are the tasks in your list:\n");
                 for (int i = 0; i < list.size(); i++){
-                    System.out.println((i + 1) + ". " + list.get(i));
+                    Task current = list.get(i);
+                    System.out.println("    " + (i + 1) + ". [" + current.getStatusIcon() + "] "
+                            + current.getDescription());
                 }
                 System.out.println("    ____________________________________________________________\n");
+            } else if (command.startsWith("mark") || command.startsWith("unmark")) {
+                String[] splitString = command.split("\\s");
+                int index = Integer.parseInt(splitString[1]);
+                System.out.println("    ____________________________________________________________\n");
+                if (command.startsWith("mark")) {
+                    list.get(index - 1).setDone();
+                    System.out.println("    Nice! I've marked this task as done: \n");
+                } else {
+                    list.get(index - 1).setNotDone();
+                    System.out.println("    OK, I've marked this task as not done yet: \n");
+                }
+                System.out.println("      [" + list.get(index - 1).getStatusIcon() + "] "
+                        + list.get(index - 1).getDescription());
+                System.out.println("    ____________________________________________________________\n");
             } else {
-            list.add(command);
+            Task t = new Task(command);
+            list.add(t);
             System.out.println("    ____________________________________________________________\n");
             System.out.println("    added:" + command);
             System.out.println("    ____________________________________________________________\n");
