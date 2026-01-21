9a2d91dc7e5236b1053a7ec8ae50842363c7a9e0
Chan Jan Hoe <e1398771@u.nus.edu>
2026-01-22 00:27:34 +0800

    Added level 2

--- src/main/java/Tigger.java
+++ src/main/java/Tigger.java
@@ -1,5 +1,6 @@
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Tigger {
     public static void main(String[] args) {
@@ -8,13 +9,24 @@ public class Tigger {
         System.out.println("What can I do for you?\n");
         System.out.println("____________________________________________________________\n");
 
+        ArrayList<String> list = new ArrayList<>();
+
         Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
 
         while (!Objects.equals(command, "bye")) {
+            if (command.equals("list")){
+                System.out.println("    ____________________________________________________________\n");
+                for (int i = 0; i < list.size(); i++){
+                    System.out.println((i + 1) + ". " + list.get(i));
+                }
+                System.out.println("    ____________________________________________________________\n");
+            } else {
+            list.add(command);
             System.out.println("    ____________________________________________________________\n");
-            System.out.println("    " + command);
+            System.out.println("    added:" + command);
             System.out.println("    ____________________________________________________________\n");
+            }
             command = scanner.nextLine();
         }
         System.out.println("    ____________________________________________________________\n");
